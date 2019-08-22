993bb561b07e4dbd752ec08ece65e2946f669be9
Ryan Toole <ryant523@gmail.com>
2019-08-22 17:50:37 -0400

    added test for large file

--- test/test.js
+++ test/test.js
@@ -3,19 +3,19 @@
 const assert = require('assert');
 const fs = require('fs');
 const SzddExpandStream = require('../index');
+const ncp = require('ncp');
 
 
 describe('SzddExpandStream', () => {
     let actualFileBuffer;
     let wrongFileBuffer;
 
+    let actualLargeFileBuffer;
+
     before(function() {
-        fs.readFile('test/test1_actual.txt', (err, contents) => {
-            actualFileBuffer = contents;
-        });
-        fs.readFile('test/test.js', (err, contents) => {
-            wrongFileBuffer = contents;
-        });
+        actualFileBuffer = Buffer.from(fs.readFileSync('test/test1_actual.txt'));
+        wrongFileBuffer = Buffer.from(fs.readFileSync('test/test.js'));
+        actualLargeFileBuffer = Buffer.from(fs.readFileSync('test/test2_actual.txt'))
     });
     it('should expand file if in Szdd format', () => {
         let output = Buffer.alloc(0);
@@ -39,5 +39,30 @@ describe('SzddExpandStream', () => {
             .on('end', () => {
                 assert.strictEqual(Buffer.compare(output, actualFileBuffer), 0);
             });
+    });
+    it('should work for large files', (done) => {
+        let output = Buffer.alloc(0);
+        fs.createReadStream('test/test2.txt')
+            .pipe(new SzddExpandStream())
+            .on('data', data => {
+                output = Buffer.concat([output, data]);
+            })
+            .on('end', () => {
+                assert.strictEqual(Buffer.compare(output, actualLargeFileBuffer), 0);
+                done();
+            });
+    });
+    it('should work with ncp', (done) => {
+        ncp('./test/test2.txt', './test/delete.txt', {
+            transform: (read, write, file) => {
+                read.pipe(new SzddExpandStream()).pipe(write);
+            }
+        }, (err) => {
+            const expandedFile = fs.readFileSync('./test/delete.txt');
+            fs.unlink('./test/delete.txt', (err) => {
+                assert.strictEqual(Buffer.compare(expandedFile, actualLargeFileBuffer), 0);
+                done()
+            });
+        });
     });
 });
